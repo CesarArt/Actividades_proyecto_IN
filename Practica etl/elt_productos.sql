47fc13d9d99d32ad11d562bf775ddfedf5f2890e
BaezCrdrm <dev.baez_crdrm@outlook.com>
2017-11-23 21:56:46 -0600

    ETL productos

---
+++ Practica etl/elt_productos.sql
@@ -0,0 +1,21 @@
+CREATE PROCEDURE etlProductos()
+BEGIN
+DROP TABLE IF EXISTS productos;
+CREATE TABLE productos(
+    id_producto VARCHAR(20),
+    descripcion_producto VARCHAR(50),
+    id_categoria CHAR(6),
+    descripcion_categoria CHAR(20));
+
+INSERT INTO dw_nova.productos
+SELECT
+    sm.stockid AS 'id_producto',
+    sm.description AS 'descripcion_producto',
+    sc.categoryid AS 'id_categoria',
+    sc.categorydescription AS 'descripcion_categoria'
+FROM infofesc_werp.stockmaster sm
+INNER JOIN infofesc_werp.stockcategory sc
+ON sm.categoryid = sc.categoryid
+GROUP BY
+1,2,3,4;
+END;
